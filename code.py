e93dcff2b499acbf8c251e974e85b3f7caeb97c3
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-11-16 12:40:52 +0530

    Comments in Python

--- code.py
+++ code.py
@@ -1,7 +1,13 @@
+'''
+Multi Line
+Comment
+'''
 name = "Shivansh Tandon"
 age = 26
 PI = 3.14
 isPrime = True
 
+#This is a Comment
+# Data Types in python
 print(type(isPrime))
 print(type(name),type(age),type(PI))
