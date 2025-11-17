6909aa215465260838f2580a4a0a9212672a206d
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-11-17 13:04:28 +0530

    arithmetic operators

--- code.py
+++ code.py
@@ -1,17 +1,10 @@
-'''
-Multi Line
-Comment
-'''
-name = "Shivansh Tandon"
-age = 26
-PI = 3.14
-isPrime = True
+#Arithmetic operators
+a = 10
+b = 5
 
-#This is a Comment
-# Data Types in python
-print(type(isPrime))
-print(type(name),type(age),type(PI))
-
-# Style Guide snake_case - prefered in python
-tot_price = 2000
-print(tot_price)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
