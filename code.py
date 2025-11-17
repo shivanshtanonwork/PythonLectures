33a3d369cd8be3819e8f97b481c9607fbf512e2e
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-11-17 14:07:34 +0530

    relational/conditional statement

--- code.py
+++ code.py
@@ -1,10 +1,8 @@
-#Arithmetic operators
+#Relational operators
 a = 10
 b = 5
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
-print(a**b)
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
