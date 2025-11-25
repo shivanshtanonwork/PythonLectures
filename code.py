36a33897940c27f786ebbe2411fe6bcf22f36e53
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-11-25 13:18:34 +0530

    average of 2 nos.

--- code.py
+++ code.py
@@ -1,21 +1,28 @@
-#Taking User Input
+# #Taking User Input
 
-a = input("Enter a value of a: ")
-print(a)
+# a = input("Enter a value of a: ")
+# print(a)
 
-user_name = input("Enter your name : ")
-print("Welcome",user_name)
+# user_name = input("Enter your name : ")
+# print("Welcome",user_name)
 
-#Sum of 2 nums
+# #Sum of 2 nums
 
-x = input("enter x : ")
-y = input("enter y : ")
+# x = input("enter x : ")
+# y = input("enter y : ")
 
-sum = x + y
-print(sum)
+# sum = x + y
+# print(sum)
 
-x = int(input("enter x : "))
-y = int(input("enter y : "))
+# x = int(input("enter x : "))
+# y = int(input("enter y : "))
 
-sum = x + y 
-print(sum)
+# sum = x + y 
+# print(sum)
+
+# #WAP to print the average of 2 nos.
+num1 = float(input("Enter 1st num : "))
+num2 = float(input("Enter 2nd num : "))
+
+average = (num1 + num2)/2
+print(average)
