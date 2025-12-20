0e384de999bac17d9bb387a95044f48c4b5f2bb2
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-12-20 16:18:37 +0530

    while loop

--- codePart2.py
+++ codePart2.py
@@ -1,13 +1,28 @@
-# Match Case in Python
+# Loops with While
+## while loop
 
-color = input("Enter color : ")
+count = 1 #iterator
+while count <= 5:
+    print("Hello Python")
+    count+=1
 
-match color:
-    case "green":
-        print("Go")
-    case "yellow":
-        print("Look")
-    case "red":
-        print("Stop")
-    case _:    #Default case
-        print("Wrong color")
+
+#count 1 to 5
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i+=1
+
+# reverse count 
+i = 5
+while i >= 1:
+    print(i)
+    i-=1
+
+# multiplication table of any no. n
+n = int(input("Enter any number "))
+
+x = 1
+while x <= 10:
+    print(n * x)
+    x+=1
