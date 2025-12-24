1a27a733485fc57bca0efcaf367d3a204f2d886e
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-12-24 15:05:01 +0530

    for loop

--- codePart2.py
+++ codePart2.py
@@ -1,26 +1,20 @@
-# break and continue
+# For loops
 
-i = 1
-while (i<=10):
-    if(i%6==0):
-        break
-    print(i)
-    i+=1 
-        
-print()
+string = "Hello"
 
-x = 1
-while(x<=10):
-    if(x%3==0):
-        x+=1
-        continue
-    print(x)
-    x+=1
+if 'o' in string:
+    print("Yes o exists in string")
 
-# Print all odd nums 1 to 10
-n = 0
-while(n<10):
-    n+=1
-    if(n%2==0):
-        continue
-    print(n)
+for var in string:
+    print(var)
+
+for i in range(5):
+    print(i+1)
+
+word = "artificial intelligence"
+#count the number of i's 
+count = 0
+for ch in word:
+    if(ch == "i"):
+        count+=1
+print("count of i = ",count)
