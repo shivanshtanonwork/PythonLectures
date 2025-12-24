21b16109e8581142d69e22b5eaf0f145b16415d3
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-12-24 16:25:58 +0530

    Functions in Python

--- codePart2.py
+++ codePart2.py
@@ -1,7 +1,20 @@
-# sum of 'n' natural numbers
+# Functions
 
-n = int(input("Enter any natural number : "))
-sum = 0
-for i in range(1,n+1):
-    sum+=i
-print(sum)
+def hello(): 
+    print("Hello Python")
+
+hello()
+
+def sum(a,b):
+    sum=a+b
+    return sum
+
+print(sum(2,3))
+
+# Print average of 3 nos.
+def avg(a,b,c=1):
+    average = (a+b+c)/3
+    return average
+
+print(avg(4,5))
+
